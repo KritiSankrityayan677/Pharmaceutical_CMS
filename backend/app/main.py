e475bcbfb7423b1b6fc7491c425b3b4805d2d80f
Kriti Pandey <kritipandey689@gmail.com>
2026-07-29 18:43:37 +0530

    chore: railway deployment config (Procfile, nixpacks, CORS)

--- backend/app/main.py
+++ backend/app/main.py
@@ -23,7 +23,11 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[settings.frontend_origin, "http://localhost:3000"],
+    allow_origins=[
+        settings.frontend_origin,
+        "http://localhost:5173",
+        "http://localhost:3000",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
